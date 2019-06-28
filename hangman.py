2c0e8f640b5dd1dec9acaf25350649b5283b81ac
bregjemastenbroek <45490800+bregjemastenbroek@users.noreply.github.com>
2019-06-28 09:44:55 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ print ("\033[1;36;40m                                                           
 print ("Uitleg: Er verschijnen straks streepjes op uw beeld. Vervolgens krijgt u de mogelijkheid een letter te kiezen, waarvan u denkt dat de letter in het woord zit. Bij een goed gekozen letter komt hij op de juiste plek in het woord te staan. Bij een foutgekozen letter komt hij ergens anders op het scherm te staan en is er 1 van je 10 beurten voorbij. Zodra je alle letters, binnen je 10 beurten, hebt geraden, heb je gewonnen. Heb je niet het woord binnen 10 beurten geraden, heb je verloren.                                \033[1;36;40m \n")
 
 def get_guess():
-  
+  foute_letters = []
   dashes = "-"* len(secret_word)
   guesses_left = 10 
   
@@ -27,8 +27,10 @@ def get_guess():
     
     else:
       print ("Deze letter zit niet in het woord! ")
+      foute_letters.append(guess)
+      print (foute_letters)
       guesses_left -= 1
-    
+ 
   if guesses_left < 0:
     print ("Je hebt verloren, het woord was: " + str(secret_word))
   
